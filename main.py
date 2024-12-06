151fab2030da7244a666dd4f6010db820889c610
SamEllis-CP3 <samuel.ellis@ucas-edu.net>
2024-12-06 11:37:12 -0700

    fixed the game

--- main.py
+++ main.py
@@ -16,10 +16,6 @@ class Figure:
     x = 0
     y = 0
 
-    def __init__(self,x, y):
-        self.x = x
-        self.y = y
-
     figures = [
         [[1, 5, 9, 13], [4, 5, 6, 7]],
         [[4, 5, 9, 10], [2, 6, 5, 9]],
@@ -30,11 +26,18 @@ class Figure:
         [[1, 2, 5, 6]],
     ]
 
-    def rotate(self):
-        self.rotation = (self.rotation - 1) % len(self.figures[self.type])
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.type = random.randint(0, len(self.figures) - 1)
+        self.color = random.randint(1, len(colors) - 1)
+        self.rotation = 0
 
     def image(self):
         return self.figures[self.type][self.rotation]
+
+    def rotate(self):
+        self.rotation = (self.rotation - 1) % len(self.figures[self.type])
 
 class Tetris:
     def __init__(self, height, width):
@@ -98,7 +101,7 @@ class Tetris:
                             j + self.figure.x < 0 or \
                             self.field[i + self.figure.y][j + self.figure.x] > 0:
                         intersection = True
-        return intersecion
+        return intersection
 
 
     def break_lines(self):
@@ -123,5 +126,97 @@ class Tetris:
             for j in range(4):
                 if i * 4 + j in self.figure.image():
                     self.field[i + self.figure.y][j + self.figure.x] = self.figure.color
+        self.break_lines()
+        self.new_figure()
+        if self.intersects():
+            self.state = "gameover"
 
 
+pygame.init()
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GRAY = (0, 81, 105)
+
+size = (400,500)
+screen = pygame.display.set_mode(size)
+
+done = False
+clock = pygame.time.Clock()
+fps = 30
+game = Tetris(18,10)
+counter = 0
+
+pygame.display.set_caption("Tetris")
+
+pressing_down = False
+
+while not done:
+    if game.figure is None:
+        game.new_figure()
+    counter += 1
+    if counter > 100000:
+        counter = 0
+
+    if counter % (fps // game.level // 2) == 0 or pressing_down:
+        if game.state == "start":
+            game.go_down()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                game.rotate()
+                pressing_down = False
+            if event.key == pygame.K_DOWN:
+                pressing_down = True
+            if event.key == pygame.K_LEFT:
+                game.go_side(-1)
+                pressing_down = False
+            if event.key == pygame.K_RIGHT:
+                game.go_side(1)
+                pressing_down = False
+            if event.key == pygame.K_SPACE:
+                game.go_space()
+                pressing_down = False
+            if event.key == pygame.K_ESCAPE:
+                game.__init__(20, 10)
+    if event.type == pygame.KEYUP:
+        if event.key == pygame.K_DOWN:
+            pressing_down = False
+    
+    screen.fill(WHITE)
+
+    for i in range(game.height):
+        for j in range(game.width):
+            pygame.draw.rect(screen, GRAY, [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
+            if game.field[i][j] > 0:
+                pygame.draw.rect(screen, colors[game.field[i][j]],
+                                 [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
+                
+    if game.figure is not None:
+        for i in range(4):
+            for j in range(4):
+                p = i * 4 + j
+                if p in game.figure.image():
+                    pygame.draw.rect(screen, colors[game.figure.color],
+                                     [game.x + game.zoom * (j + game.figure.x) + 1,
+                                      game.y + game.zoom * (i + game.figure.y) + 1,
+                                      game.zoom - 2, game.zoom - 2])
+                    
+    font = pygame.font.SysFont('Retro', 25, True, False)
+    font1 = pygame.font.SysFont('Retro', 65, True, False)
+    text = font.render("Score: " + str(game.score), True, BLACK)
+    text_game_over = font1.render("Game Over", True, BLACK)
+    text_game_over1 = font1.render("Press ESC", True, BLACK)
+
+    screen.blit(text, [0, 0])
+    if game.state == "gameover":
+        screen.blit(text_game_over, [20, 200])
+        screen.blit(text_game_over1, [25, 265])
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+pygame.quit()
